17bab0b2a0476f713be91ee7466a5b849393d6a4
jeongdopark <jdpark0816@gmail.com>
2023-05-11 20:48:56 +0900

    feat: error handle

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,5 +1,7 @@
-import axios, { InternalAxiosRequestConfig } from 'axios'
+import axios, { InternalAxiosRequestConfig, AxiosError } from 'axios'
 import token from '../util/token'
+import { ACCESS_TOKEN } from '../util/constants'
+import { APIError } from '../@types/api'
 export const instance = axios.create({
   baseURL: 'http://52.79.226.246',
   headers: { 'Content-Type': 'application/json' },
@@ -7,7 +9,7 @@ export const instance = axios.create({
 
 instance.interceptors.request.use(
   (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
-    const accesstoken = token.get()
+    const accesstoken = token.get(ACCESS_TOKEN)
     if (accesstoken !== null)
       Object.assign(config.headers, { Authoriz: `Bearer ${accesstoken}` })
     return config
@@ -17,3 +19,13 @@ instance.interceptors.request.use(
     return Promise.reject(error)
   },
 )
+
+export const createDefaultAPIError = (error: unknown): APIError => {
+  if (error instanceof AxiosError) {
+    return {
+      isSuccess: false,
+      message: error.response?.data.message,
+    }
+  }
+  return { isSuccess: false, message: '알 수 없는 에러입니다.' }
+}

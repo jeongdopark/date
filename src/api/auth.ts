365c63f1a979540b9eef9d663a25db95ee2e204e
jeongdopark <jdpark0816@gmail.com>
2023-05-11 20:45:54 +0900

    feat: signup, signin API function

---
+++ src/api/auth.ts
@@ -0,0 +1,35 @@
+import { instance, createDefaultAPIError } from './client'
+import ApiUrl from './ApiUrl'
+import { Auth } from '../@types/auth'
+import { APIResponse, SignInResponse } from '../@types/api'
+
+export const signUp = async ({ userId, password, nickname }: Auth) => {
+  try {
+    const res = await instance.post(ApiUrl.signUp, {
+      userId,
+      password,
+      nickname,
+    })
+    return {
+      isSuccess: true,
+      data: res.data,
+    }
+  } catch (error) {
+    return createDefaultAPIError(error)
+  }
+}
+
+export const signIn = async ({
+  userId,
+  password,
+}: Auth): Promise<APIResponse<SignInResponse>> => {
+  try {
+    const res = await instance.post(ApiUrl.signIn, { userId, password })
+    return {
+      isSuccess: true,
+      data: res.data,
+    }
+  } catch (error) {
+    return createDefaultAPIError(error)
+  }
+}

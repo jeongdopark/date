a38ed7be93b302bb5142e05a022f913315c4a1c9
jeongdopark <jdpark0816@gmail.com>
2023-05-11 20:50:01 +0900

    feat: signIn, signUp component set API

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,18 +1,29 @@
 import { S } from './style'
-import { useNavigate } from 'react-router'
 import { useValidate } from '../hook/useValidate'
+import { signUp } from '../api/auth'
+import { useNavigate } from 'react-router'
+
 const SignUpForm = () => {
-  const [Inputvalue, inputOnChange, warnMessage] = useValidate()
+  const [inputValue, inputOnChange, warnMessage] = useValidate()
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const res = await signUp(inputValue)
+    if (res.isSuccess) {
+      navigate('/signIn')
+    }
+  }
 
   return (
-    <S.FormContainer>
+    <S.FormContainer onSubmit={handleSubmit}>
       <S.Title>아이디</S.Title>
       <S.Input
-        name="email"
+        name="userId"
         onChange={inputOnChange}
         placeholder="아이디를 입력해 주세요."
       />
-      <span>{warnMessage.current.value['email']}</span>
+      <span>{warnMessage.current.value['userId']}</span>
       <S.Title>비밀번호</S.Title>
       <S.Input
         name="password"
@@ -21,7 +32,11 @@ const SignUpForm = () => {
       />
       <span>{warnMessage.current.value['password']}</span>
       <S.Title>닉네임</S.Title>
-      <S.Input placeholder="닉네임을 입력해 주세요." />
+      <S.Input
+        name="nickname"
+        onChange={inputOnChange}
+        placeholder="닉네임을 입력해 주세요."
+      />
       <span>{warnMessage.current.value['nickname']}</span>
       <S.Button>SignUp</S.Button>
     </S.FormContainer>

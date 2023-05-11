ce4f260d7a4868775bde623df0d495a895939f03
jeongdopark <jdpark0816@gmail.com>
2023-05-11 13:08:05 +0900

    feat: SignIn, SignUp components

--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -1,14 +1,26 @@
 import { S } from './style'
 import { useNavigate } from 'react-router'
+import { useValidate } from '../hook/useValidate'
 const SignInForm = () => {
   const navigate = useNavigate()
+  const [inputValue, inputOnChange, warnMessage] = useValidate()
 
   return (
     <S.FormContainer>
-      <S.Title>아이디</S.Title>
-      <S.Input placeholder="아이디를 입력해 주세요." />
+      <S.Title>이메일</S.Title>
+      <S.Input
+        name="email"
+        onChange={inputOnChange}
+        placeholder="아이디를 입력해 주세요."
+      />
+      <span>{warnMessage.current.value['email']}</span>
       <S.Title>비밀번호</S.Title>
-      <S.Input placeholder="비밀번호를 입력해 주세요." />
+      <S.Input
+        name="password"
+        onChange={inputOnChange}
+        placeholder="비밀번호를 입력해 주세요."
+      />
+      <span>{warnMessage.current.value['password']}</span>
       <S.Button>Login</S.Button>
       <S.Button onClick={() => navigate('/signUp')}>Go Sign Up</S.Button>
     </S.FormContainer>
--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,14 +1,26 @@
 import { S } from './style'
 import { useNavigate } from 'react-router'
+import { useValidate } from '../hook/useValidate'
 const SignUpForm = () => {
   const navigate = useNavigate()
+  const [Inputvalue, inputOnChange, warnMessage] = useValidate()
 
   return (
     <S.FormContainer>
       <S.Title>아이디</S.Title>
-      <S.Input placeholder="아이디를 입력해 주세요." />
+      <S.Input
+        name="email"
+        onChange={inputOnChange}
+        placeholder="아이디를 입력해 주세요."
+      />
+      <span>{warnMessage.current.value['email']}</span>
       <S.Title>비밀번호</S.Title>
-      <S.Input placeholder="비밀번호를 입력해 주세요." />
+      <S.Input
+        name="password"
+        onChange={inputOnChange}
+        placeholder="비밀번호를 입력해 주세요."
+      />
+      <span>{warnMessage.current.value['password']}</span>
       <S.Title>닉네임</S.Title>
       <S.Input placeholder="닉네임을 입력해 주세요." />
       <S.Button>Sign Up</S.Button>

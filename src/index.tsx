662487e8065090504ee517f88bfb27b409d49f6c
jeongdopark <jdpark0816@gmail.com>
2023-05-11 13:07:41 +0900

    feat: App Component

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { ResetStyle } from "./style/reset.styled";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { ResetStyle } from './style/reset.styled'
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
+  <React.Fragment>
     <ResetStyle />
     <App />
-  </React.StrictMode>
-);
+  </React.Fragment>,
+)

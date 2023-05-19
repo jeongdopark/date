0bed8179968c404d4672831dda092437435e2229
jeongdopark <jdpark0816@gmail.com>
2023-05-19 20:03:06 +0900

    feat: mypage에서 Sendlist 페이지 이동

--- src/pages/LikeList.tsx
+++ src/pages/LikeList.tsx
@@ -1,14 +1,27 @@
 import useTab from '../hook/useTab'
+import { useLocation } from 'react-router'
 import { S } from './style'
+import { useEffect } from 'react'
 
 const LikeList = () => {
   const { tabIndex, onChangeTabIndex, tab_obj } = useTab()
+  const { state: isLocation } = useLocation()
+
+  // Mypage에서 '내가 좋아하는 사람' or '나를 좋아하는 사람'버튼을 클릭하고 진입할 시에 if문 동작
+  // 분리할 수 있는지 고민.
+  useEffect(() => {
+    if (isLocation !== null) onChangeTabIndex(isLocation.tabIdx)
+  }, [])
 
   return (
     <S.Container>
       <S.TabContainer>
         {tab_obj.map((_, idx) => (
-          <S.TabElement key={idx} onClick={() => onChangeTabIndex(idx)}>
+          <S.TabElement
+            isSelected={idx === tabIndex}
+            key={idx}
+            onClick={() => onChangeTabIndex(idx)}
+          >
             {tab_obj[idx].name}
           </S.TabElement>
         ))}
--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,10 +1,15 @@
 import { S } from './style'
 import { useUserInfo } from '../hook/useUserInfo'
 import { useEffect } from 'react'
+import { useNavigate } from 'react-router'
 import UserImg from '../components/Mypage/UserImg'
+import useLogout from '../hook/useLogout'
+import { SEND_IDX, RECEIVE_IDX } from '../util/constants'
 
 const MyPage = () => {
   const { getUserInfos, userNickname, userImg, userId } = useUserInfo()
+  const navigate = useNavigate()
+  const { logout } = useLogout()
 
   useEffect(() => {
     getUserInfos()
@@ -13,9 +18,29 @@ const MyPage = () => {
   return (
     <S.Container>
       <UserImg userImg={userImg} userNickname={userNickname} />
-      <S.Button>내가 좋아하는 사람</S.Button>
-      <S.Button>나를 좋아하는 사람</S.Button>
-      <S.Button>로그아웃</S.Button>
+      <S.Button
+        onClick={() =>
+          navigate('/likelist', {
+            state: {
+              tabIdx: SEND_IDX,
+            },
+          })
+        }
+      >
+        내가 좋아하는 사람
+      </S.Button>
+      <S.Button
+        onClick={() =>
+          navigate('/likelist', {
+            state: {
+              tabIdx: RECEIVE_IDX,
+            },
+          })
+        }
+      >
+        나를 좋아하는 사람
+      </S.Button>
+      <S.Button onClick={logout}>로그아웃</S.Button>
     </S.Container>
   )
 }

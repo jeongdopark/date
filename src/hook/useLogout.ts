c4ea3f393a784c44fcceed89aff25bf5f06bf93a
jeongdopark <jdpark0816@gmail.com>
2023-05-19 20:01:34 +0900

    feat: logout hook

---
+++ src/hook/useLogout.ts
@@ -0,0 +1,19 @@
+import { useNavigate } from 'react-router'
+import storage from '../util/localStorage'
+import { REFRESH_TOKEN, ACCESS_TOKEN } from '../util/constants'
+import { toast } from 'react-toastify'
+
+const useLogout = () => {
+  const navigate = useNavigate()
+
+  const logout = () => {
+    storage.remove(REFRESH_TOKEN)
+    storage.remove(ACCESS_TOKEN)
+    toast('로그아웃 완료')
+    navigate('/signin')
+  }
+
+  return { logout }
+}
+
+export default useLogout

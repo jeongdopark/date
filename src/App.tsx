d479b6fcaf06e25a62d20e1932987c71a4e4cebf
jeongdopark <jdpark0816@gmail.com>
2023-05-21 01:22:14 +0900

    refactor: createBrowerRouter 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import AppRouter from './routes'
+import router from './router'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import { S } from './style/center.styled'
+import { RouterProvider } from 'react-router'
 
 function App() {
   return (
     <React.Fragment>
       <ToastContainer position="top-right" autoClose={1500} />
-      <AppRouter />;
+      <S.Center>
+        <RouterProvider router={router} />
+      </S.Center>
     </React.Fragment>
   )
 }
---
+++ src/router/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { createBrowserRouter } from 'react-router-dom'
+import SignIn from '../pages/SignIn'
+import SignUp from '../pages/SignUp'
+import MyPage from '../pages/MyPage'
+import LikeList from '../pages/LikeList'
+import Choice from '../pages/Choice'
+import Main from '../pages/Main'
+import Auth from './Auth'
+import { PATH_NAME } from '../util/constants'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Auth />,
+    children: [
+      {
+        path: '',
+        element: <Main />,
+      },
+      {
+        path: PATH_NAME.SIGN_IN,
+        element: <SignIn />,
+      },
+      {
+        path: PATH_NAME.SIGN_UP,
+        element: <SignUp />,
+      },
+      {
+        path: PATH_NAME.MYPAGE,
+        element: <MyPage />,
+      },
+      {
+        path: PATH_NAME.LIKELIST,
+        element: <LikeList />,
+      },
+      {
+        path: PATH_NAME.CHOICE,
+        element: <Choice />,
+      },
+    ],
+  },
+])
+
+export default router

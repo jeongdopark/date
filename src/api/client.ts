33d9f7578cda06af0364b4e9b01a268e6d717b72
jeongdopark <jdpark0816@gmail.com>
2023-05-20 17:55:47 +0900

    feat: 토큰 만료interceptor로 refresh

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,9 @@
 import axios, { InternalAxiosRequestConfig, AxiosError } from 'axios'
 import token from '../util/token'
+import storage from '../util/localStorage'
 import { ACCESS_TOKEN } from '../util/constants'
 import { APIError } from '../@types/api'
+import ApiUrl from './ApiUrl'
 
 export const instance = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
@@ -17,6 +19,47 @@ instance.interceptors.request.use(
   },
 
   (error) => {
+    return Promise.reject(error)
+  },
+)
+
+instance.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    // console.log('get response', response)
+    return response
+  },
+  async (error) => {
+    const {
+      config,
+      response: { status },
+    } = error
+    if (status === 419) {
+      if (error.response.data.message === 'expired') {
+        const originalRequest = config
+        const refreshToken = localStorage.getItem('refreshToken')
+        // token refresh 요청
+        const { data } = await instance.post(
+          ApiUrl.refreshToken, // token refresh api
+          { refresh: refreshToken },
+          { headers: { authorization: refreshToken } },
+        )
+        // 새로운 토큰 저장
+        // dispatch(userSlice.actions.setAccessToken(data.data.accessToken)); store에 저장
+        const { accessToken: newAccessToken } = data
+        storage.set('accessToken', newAccessToken)
+        originalRequest.headers.authorization = newAccessToken
+        alert('refresh')
+        console.log('refresh')
+
+        // 401로 요청 실패했던 요청 새로운 accessToken으로 재요청
+        return axios(originalRequest)
+      }
+    }
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    console.log('response error', error)
     return Promise.reject(error)
   },
 )

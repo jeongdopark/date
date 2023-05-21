ce870efb47d5f2bc925e35e7cf2e4b7a2a687ab7
jeongdopark <jdpark0816@gmail.com>
2023-05-21 20:03:30 +0900

    feat: 유효성 검사 추가

--- src/components/Mypage/UpdateModal.tsx
+++ src/components/Mypage/UpdateModal.tsx
@@ -2,6 +2,7 @@ import { updateUserInfo } from '../../api/user'
 import { useInput } from '../../hook/useInput'
 import { S } from '../style'
 import { NICKNAME, PROFILE_IMG_URL } from '../../util/constants'
+import { useValidate } from '../../hook/useValidate'
 
 const UpdateModal = ({
   userNickname,
@@ -15,6 +16,8 @@ const UpdateModal = ({
   setTrigger: React.Dispatch<React.SetStateAction<boolean>>
 }) => {
   const { inputValue, onChange } = useInput(userNickname)
+  const [nickNameValue, OnChangeNickName, warnMessage, valid] = useValidate()
+  console.log(nickNameValue, warnMessage, valid)
 
   const handleUpdateUserInfo = async () => {
     try {
@@ -36,10 +39,11 @@ const UpdateModal = ({
         <S.Title>닉네임 변경</S.Title>
         <S.Input
           name={NICKNAME}
-          onChange={onChange}
-          value={inputValue.nickname}
+          onChange={OnChangeNickName}
+          value={nickNameValue[NICKNAME]}
           placeholder="닉네임 변경하기"
         />
+        <S.WarnMessage>{warnMessage.current.value[NICKNAME]}</S.WarnMessage>
         <S.Title>프로필 이미지 변경</S.Title>
         <S.Input
           name={PROFILE_IMG_URL}
@@ -47,7 +51,9 @@ const UpdateModal = ({
           style={{ marginBottom: '30px' }}
           onChange={onChange}
         />
-        <S.Button onClick={handleUpdateUserInfo}>내 정보 변경하기</S.Button>
+        <S.Button disabled={valid} onClick={handleUpdateUserInfo}>
+          내 정보 변경하기
+        </S.Button>
       </S.ModalInnerContainer>
     </S.ModalContainer>
   )

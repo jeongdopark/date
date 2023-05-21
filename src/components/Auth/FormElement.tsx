aee6129ddc6a38df8d52ce10f29644048a7cc978
jeongdopark <jdpark0816@gmail.com>
2023-05-21 16:45:18 +0900

    refactor: 불필요한 코드 수정

--- src/components/Auth/FormElement.tsx
+++ src/components/Auth/FormElement.tsx
@@ -20,26 +20,13 @@ const FormElement = ({
   type: ElementType
   warnMessage: React.MutableRefObject<WarnMsgType>
 }) => {
-  //FIXME
-  const typeObj = {
-    userId: {
-      title: 'ID',
-    },
-    password: {
-      title: 'Password',
-    },
-    nickname: {
-      title: 'Nickname',
-    },
-  }
-
   return (
     <React.Fragment>
-      <S.Title>{typeObj[type].title}</S.Title>
+      <S.Title>{type}</S.Title>
       <S.Input
         name={type}
         onChange={inputOnChange}
-        placeholder={`${typeObj[type].title} 입력해 주세요.`}
+        placeholder={`${type} 입력해 주세요.`}
       />
       <S.WarnMessage>{warnMessage.current.value[type]}</S.WarnMessage>
     </React.Fragment>

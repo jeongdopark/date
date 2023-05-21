fbcf2c7a433151b0e022f15810e41b7b4fedb863
jeongdopark <jdpark0816@gmail.com>
2023-05-21 16:32:43 +0900

    fix: add loading state

--- src/hook/useGetUserList.ts
+++ src/hook/useGetUserList.ts
@@ -5,8 +5,11 @@ import { patchLike, patchDisLike } from '../api/affinity'
 
 // FIXME
 const useGetUserList = () => {
+  // like, dislike api 호출 시 업데이트 된 userList를 받아오기 위해 trigger 변수 설정.
+  const [trigger, setTrigger] = useState<boolean>(false)
+
   const [userList, setUserList] = useState<UserInfo[]>([])
-  const [trigger, setTrigger] = useState(false)
+  const [loading, setLoading] = useState<boolean>(false)
   const [currentIndex, setCurrentIndex] = useState<number>(0)
   const [currentUser, setCurrentUser] = useState<UserInfo>({
     nickname: '',
@@ -36,16 +39,19 @@ const useGetUserList = () => {
     }
   }
 
-  // like/dislike 동작 이후에 새롭게 업데이트 된 리스트 불러오기
+  // like/dislike 동작 이후에 새롭게 업데이트 된 리스트 불러오기 , trigger에 의존
   useEffect(() => {
     const fetch = async () => {
       try {
+        setLoading(true)
         const res = await getUserList()
         // 리스트의 마지막 유저일 경우
         if (currentIndex === userList.length - 1) setCurrentIndex(0)
         setUserList([...res.data])
       } catch (error) {
         console.log(error)
+      } finally {
+        setLoading(false)
       }
     }
     fetch()
@@ -65,6 +71,7 @@ const useGetUserList = () => {
     setCurrentUser,
     currentIndex,
     setCurrentIndex,
+    loading,
   }
 }
 
--- src/pages/Choice.tsx
+++ src/pages/Choice.tsx
@@ -1,8 +1,6 @@
 import { S } from './style'
-import { useState, useEffect } from 'react'
 import Carousel from '../components/Choice/Carousel'
 import Button from '../components/Choice/Button'
-import { UserInfo } from '../@types/api'
 import useGetUserList from '../hook/useGetUserList'
 
 const Choice = () => {
@@ -14,12 +12,23 @@ const Choice = () => {
     setCurrentUser,
     currentIndex,
     setCurrentIndex,
+    loading,
   } = useGetUserList()
 
-  if (userList.length === 0) {
+  // 유저 불러오기 로딩
+  if (loading && userList.length === 0) {
     return (
       <S.Container>
         <div>Loading</div>
+      </S.Container>
+    )
+  }
+
+  // 유저리스트가 비어있을 경우
+  if (userList.length === 0) {
+    return (
+      <S.Container>
+        <div>더이상 유저가 없습니다</div>
       </S.Container>
     )
   }

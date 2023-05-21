4c911e2b5e90ee7ff3766cc65fc5bf901a686bde
jeongdopark <jdpark0816@gmail.com>
2023-05-21 20:05:44 +0900

    add: constant

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -7,7 +7,8 @@ export const NICKNAME = 'nickname'
 export const PROFILE_IMG_URL = 'profileImgUrl'
 export const USER_ID = 'userId'
 export const PASSWORD = 'password'
-
+export const SIGN_IN = 'signin'
+export const SIGN_UP = 'signup'
 export const PATH_NAME = {
   SIGN_IN: '/signin',
   SIGN_UP: '/signup',
